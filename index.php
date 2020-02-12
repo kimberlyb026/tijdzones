623738718350b0c7fa83e475b8f119f9f5fd42e1
kimberly <99051407@mydavinci.nl>
2020-02-12 09:58:51 +0100

    1

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+$time = strtotime('+1 hour');
+
+strtotime('+ 1 hour', $time);
+
+$time = date('H:i', strtotime('+1 hour'));
+
+
+
+
+if($time < 6 ){
+	$img = "night.png";
+	$text =" Goede Nacht!";
+}
+else if($time <12 && 6 < $time){
+	$img = "morning.png";
+	$text ="Goede Morgen!";
+}
+
+else if ($time <18 && 12 < $time){
+	$img = "afternoon.png";
+	$text ="Goede Middag! ";
+}
+
+else if ($time <24 && 18 < $time){
+	$img = "evening.png";
+	$text ="Goede Avond! ";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+    	<title></title>
+    	<link rel="stylesheet" type="text/css" href="./style.css"></head>
+    	<link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah&display=swap" rel="stylesheet">
+	</head>
+	<body>   
+		<img src="<?php echo $img;  ?>"> 
+<div class="test">
+    <h1><?php echo $text;  ?></h1>
+    <h1>Het is nu <?php echo $time; ?></h1>
+
+    </div>
+
+    </body>
+</html>
